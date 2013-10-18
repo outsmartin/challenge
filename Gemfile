0cba57de7be3fc795d3fd6c7c9527c43ce1e58da
Martin Schneider <info@outsmartin.de>
2013-10-18 14:30:47 +0200

    Initial Rails Setup

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem "rails"
+# Experimental Bootstrap 3.0 beta -- use at your own risk.
+gem 'sass-rails', '>= 3.2'
+gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass'
+gem "minitest-rails"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Challenge::Application.config.secret_key_base = '5b2acee6c77e2dbb1e4554d885a50dc0f6bdfc88b876ef6682f484ec9343cc306a5c0741647ba32dd574f7ad065139f67f1f1fb8d3c18432a0643b6accfb981c'
